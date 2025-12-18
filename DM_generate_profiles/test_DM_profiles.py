b22a5085e724e521de6e48d9bfcb3ad51739287c
Kgamlin <kai.gamlin@orange.fr>
2025-12-18 11:45:14 -0700

    added test section for zernike polynomials

--- DM_generate_profiles/test_DM_profiles.py
+++ DM_generate_profiles/test_DM_profiles.py
@@ -1,5 +1,5 @@
 from DM_generate_Profile import DMShape
-
+from numpy import pi
 
 if __name__ == "__main__":
 
@@ -18,3 +18,24 @@ if __name__ == "__main__":
     dm =DMShape(config=params)
     # ---- THE ONLY USER CALL ----
     dm.generate_gradient_file(k_lambda=6)
+
+    config = {
+            "n_actuators": 137,
+            "stroke": 1.5e-6,
+            "wavelength": 514e-9,
+            "N": 13,
+            "paths": {
+                "directory": "output_shapes",
+                "filename": "zernike_n2_m0_v2.csv"
+            }
+        }
+
+    dm = DMShape(config)
+
+    # Defocus with π phase peak
+    dm.generate_zernike_file(
+        n=2,
+        m=0,
+        amplitude_rad=2*pi,
+        radius_actuators=6.5
+    )
